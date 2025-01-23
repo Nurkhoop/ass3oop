0f1ed70d071fa1d01a84804ea4248f3231102ccb
Nurkhoop <kuan222221@gmail.com>
2025-01-23 18:05:48 +0600

    Delete src/Assignment_1 directory

--- src/Assignment_1/Assignment_1_1/Task1.java
+++
@@ -1,9 +0,0 @@
-package Assignment_1.Assignment_1_1;
-
-public class Task1 {
-    public static void main(String[] args) {
-        String name;
-        int age;
-        String city;
-    }
-}
--- src/Assignment_1/Assignment_1_2/Task8.java
+++
@@ -1,9 +0,0 @@
-package Assignment_1.Assignment_1_2;
-
-public class Task8 {
-    public static void main(String[] args) {
-        int sum = 0;
-        int num = 1;
-        System.out.println(sum);
-    }
-}
